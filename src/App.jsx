d9ec8af3196fb9fa90234e43dee45d15746d989b
Yorn90104 <bingxiao526@gmail.com>
2025-02-18 18:01:54 +0800

    BG & SuperCircle

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import BeginButton from './components/BeginButton'
+import "./App.css"
+import Home from "./components/Home"
+
 
 function App() {
 
 	return (
 		<>
-			<BeginButton/>
+			<Home />
 		</>
 	)
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import SuperCircle from "../assets/SuperCircle.png";
+
+function Home() {
+  return (
+    <>
+      <img src={SuperCircle}  alt="SuperCircle" />
+    </>
+  );
+}   
+
+export default Home;
