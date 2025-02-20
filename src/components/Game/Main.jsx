f0313d9053b6d0f397ec96c1ce751931f2f0a657
Yorn90104 <bingxiao526@gmail.com>
2025-02-20 21:02:36 +0800

    Begin 中的小 function

--- src/components/Game/Main.jsx
+++ src/components/Game/Main.jsx
@@ -25,7 +25,16 @@ import Handsun from "../../assets/Handsun.jpg";
 import Kachu from "../../assets/Kachu.jpg";
 import Rrr from "../../assets/RRR.jpg";
 
+import superhhh from "../../assets/super_hhh.jpg";
+import greenwei from "../../assets/green_wei.jpg";
+import GreenLeft from "../../assets/GreenLeft.jpg";
+import GreenMid from "../../assets/GreenMid.jpg";
+import GreenRight from "../../assets/GreenRight.jpg";
+import pikachu from "../../assets/pikachu.jpg";
+
 import Ding from "../../assets/Ding.mp3";
+import SuperUP from "../../assets/SuperUP.mp3";
+import GreenUP from "../../assets/GreenUP.mp3";
 
 function Sound(s) {
   const audio = new Audio(s);
@@ -79,31 +88,81 @@ function GameScreen({ setScreen }) {
   }, [NowMode]); // **當 nowMode 改變時，執行 useEffect**
 
   function Begin() {
-    setButtonAble(false);
-    
-    [setNowLP, setNowMP, setNowRP].forEach((setP) =>
-      setP(QSTs[Game.NowMode()])
-    );
-    Game.Logic();
-    setNowMarginScore(0);
+    const reset_qst_and_marginscore = () => {
+      [setNowLP, setNowMP, setNowRP].forEach((setP) =>
+        setP(QSTs[Game.NowMode()])
+      );
+      setNowMarginScore(0);
+    };
+    const change_picture_per500ms = () => {
+      for (let i = 0; i < 3; i++) {
+        setTimeout(() => {
+          setPs[i](Pictures[Game.Ps[i].code]);
+          console.log(`更新位置 ${i} 的圖片`);
+          Sound(Ding);
+        }, 500 * (i + 1));
+      }
+    };
+    const mode_picture_and_sound = () => {
+      const mode = Game.NowMode();
+      switch (mode) {
+        case "Normal":
+          break;
+        case "SuperHHH":
+          for (let i = 0; i < 3; i++) {
+            if (Game.Ps[i].code == "B") {
+              setPs[i](superhhh);
+            }
+          }
+          Sound(SuperUP);
+          break;
+        case "GreenWei":
+          if (Game.Ps.every((p) => p.code === "A")) {
+            setNowLP(GreenLeft);
+            setNowMP(GreenMid);
+            setNowRP(GreenRight);
+          } else if (Game.Ps.some((p) => p.code === "A")) {
+            for (let i = 0; i < 3; i++) {
+              if (Game.Ps[i].code == "A") {
+                setPs[i](greenwei);
+              }
+            }
+          } else {
+            setPs.forEach((setP) => {
+              setP(greenwei);
+            });
+          }
+          Sound(GreenUP);
+          break;
+        case "PiKaChu":
+          for (let i = 0; i < 3; i++) {
+            if (Game.Ps[i].code == "E") {
+              setPs[i](pikachu);
+            }
+          }
+          break;
+      }
+    };
+    const info_text = () => {
+      setNowMarginScore(Game.MarginScore);
+      setNowScore(Game.Score);
+      setNowTimes(Game.Times - Game.Played);
+    };
 
-    for (let i = 0; i < 3; i++) {
-      setTimeout(() => {
-        setPs[i](Pictures[Game.Ps[i].code]);
-        console.log(`更新位置 ${i} 的圖片`);
-        Sound(Ding);
-      }, 500 * (i + 1));
-    }
+    setButtonAble(false);
+    reset_qst_and_marginscore();
+    Game.Logic();
+    change_picture_per500ms();
 
     setTimeout(() => {
       if (Game.NowMode() !== NowMode) {
         setNowMode(Game.NowMode());
         document.body.style.backgroundImage = `url(${BGs[NowMode]})`;
       }
-
-      setNowMarginScore(Game.MarginScore);
-      setNowScore(Game.Score);
-      setNowTimes(Game.Times - Game.Played);
+      info_text();
+      if (Game.ModeToScreen) {
+        mode_picture_and_sound();
+      }
     }, 3000);
 
     setTimeout(() => {
