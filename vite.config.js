d50e91e78cbeb75b4b9cf7a68e24ac9208da0601
Yorn90104 <bingxiao526@gmail.com>
2025-02-20 13:49:36 +0800

    isGitHubPages ?

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 檢查是否是 GitHub Pages 環境
+const isGitHubPages = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-  base: "./", // 設定正確的 GitHub Pages 路徑
+  base: isGitHubPages ? "/LaBaG-JavaScript/" : "/", // 部署用倉庫名稱，本地開發用 "/"
   build: {
     outDir: "dist"
   },
-  plugins: [react()],
-})
+  plugins: [react()]
+});
