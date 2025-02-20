3ed36f079c283665d0ff74c320c2757e088eecc2
Yorn90104 <bingxiao526@gmail.com>
2025-02-20 21:04:05 +0800

    SB 路徑

--- src/components/End/ScoresPicture.jsx
+++ src/components/End/ScoresPicture.jsx
@@ -1,7 +1,8 @@
+import SB from "../../assets/SB.png"
 function ScoresPicture() {
   return (
     <>
-      <img src="/images/SB.png" style={{margin: "15px", width: "450px", height: "173px" }} />
+      <img src={SB} style={{margin: "15px", width: "450px", height: "173px" }} />
     </>
   );
 }export default ScoresPicture;
