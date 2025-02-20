8d241b54b406b118e3beee3f79c3574364c1a894
Yorn90104 <bingxiao526@gmail.com>
2025-02-20 13:22:07 +0800

    base route

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/LaBaG-JavaScript/", // 設定正確的 GitHub Pages 路徑
   build: {
     outDir: "dist"
   },
