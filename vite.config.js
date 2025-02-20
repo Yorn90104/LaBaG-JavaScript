cb70b5017855365c087ebef830508f2ffcc55b58
Yorn90104 <bingxiao526@gmail.com>
2025-02-20 12:45:51 +0800

    gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/LaBaG-JavaScript/",
+  base: "/LaBaG-JavaScript/", // 設定正確的 GitHub Pages 路徑
   build: {
     outDir: "dist"
   },
